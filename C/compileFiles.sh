d0d68c095ea9cda9c2faea04e04cfa5813f1db02
Danii47 <uranux1@gmail.com>
2024-10-06 15:48:33 +0200

    Commit automático de actualización carpeta C de FSO | 2024-10-06 15:48:33

--- C/compileFiles.sh
+++ C/compileFiles.sh
@@ -38,9 +38,9 @@ done
 if [[ "$1" == "y" || "$1" == "yes" ]]; then
     git add .
     commit_message="Commit automático de actualización carpeta C de FSO | $(date +"%Y-%m-%d %H:%M:%S")"
-    git commit -m "$commit_message"
+    git commit -m "$commit_message" > /dev/null 2>&1
     git push origin main > /dev/null 2>&1
-    echo -e "\033[36mCambios subidos automaticamente al repositorio de GitHub.\033[0m"
+    echo -e "\033[1;34mCambios subidos automaticamente al repositorio de GitHub.\033[0m"
 else
     echo -e "\033[97mCambios \033[31mNO\033[97m subidos a GitHub.\033[0m"
 fi
